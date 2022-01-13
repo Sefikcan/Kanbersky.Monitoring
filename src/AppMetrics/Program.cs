560c1a613ce18422a8418dad3654f1269ac1ef74
Şefik Can Kanber <sefikcankanber@gmail.com>
2022-01-13 23:54:41 +0300

    add prometheus and prepare alert mechanism

---
+++ src/AppMetrics/Program.cs
@@ -0,0 +1,45 @@
+using App.Metrics.AspNetCore;
+using App.Metrics.Formatters.Prometheus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Metric register
+builder.Services.AddMetrics();
+
+var app = builder.Build();
+
+builder.Host.UseMetricsWebTracking();
+builder.Host.UseMetrics(options =>
+{
+    options.EndpointOptions = endpointsOptions =>
+    {
+        endpointsOptions.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+        endpointsOptions.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
+        endpointsOptions.EnvironmentInfoEndpointEnabled = false;
+    };
+});
+
+// Register iþlemi sonrasýnda http://localhost:xxxx/metrics veya http://localhost:xxxx/metrics-text endpointine istek atýlýr.
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
